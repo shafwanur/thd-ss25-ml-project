f4f6a717eeeca32560c89267d9001a67d5d1a530
l3v-m <81872860+l3v-m@users.noreply.github.com>
2025-06-10 21:17:21 +0000

    "_"

---
+++ src/ml_project/data.py
@@ -0,0 +1,157 @@
+from functools import cache, cached_property
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from scipy.io.arff import loadarff
+
+pd.set_option("future.no_silent_downcasting", True)
+pd.set_option("mode.copy_on_write", True)
+
+
+class Dataset:
+    def __init__(self, path: Path):
+        self.path = path
+
+    @cached_property
+    def _arff(self):
+        return loadarff(self.path)
+
+    @cached_property
+    def cls_list(self):
+        return list(map(str.encode, self._arff[1]["class1"][1]))
+
+    @cached_property
+    def cls_id_map(self):
+        return {c: index + 1 for index, c in enumerate(self.cls_list)}
+
+    @cached_property
+    def cls_id_repr_map(self):
+        return {
+            index + 1: f"{c.decode()} ({index + 1})"
+            for index, c in enumerate(self.cls_list)
+        }
+
+    @cached_property
+    def raw(self):
+        df = pd.DataFrame(self._arff[0])
+        return df
+
+    @cached_property
+    def orig(self):
+        df = self.raw
+        df = df.rename(
+            columns={
+                "class1": "cls",
+                "flowPktsPerSecond": "pps",
+                "flowBytesPerSecond": "bps",
+            }
+        )
+        df["cls"] = df["cls"].replace(self.cls_id_map).astype(int)
+        return df
+
+    @cached_property
+    def with_dur(self):
+        # The rows with duration == 0 seem to be trash
+        df = self.orig
+        return df.loc[df.duration > 0]
+
+    @property
+    def mostly_present(self):
+        # These 5 features are always here (when duration > 0)
+        return [
+            "duration",
+            "pps",
+            "bps",
+            "max_flowiat",
+            "mean_flowiat",
+        ]
+
+    @cached_property
+    def numerical5(self):
+        return self.with_dur[self.mostly_present + ["cls"]]
+
+    @cached_property
+    def drop_missing(self):
+        return positive(self.orig)
+
+    @cached_property
+    def flagged(self):
+        df = self.with_dur
+
+        return pd.DataFrame(
+            {
+                "cls": df.cls,
+                "duration": df.duration,
+                "pps": df.pps,
+                "bps": df.bps,
+                "max_flowiat": df.max_flowiat,
+                "mean_flowiat": df.mean_flowiat,
+                # The *_active *_idle features are missing in more than half of the rows
+                # And their missing or being present are connected
+                "has_active": (df.max_active > 0).astype(int),
+                # The std_active and std_idle behave a bit differently from *_active
+                "has_std_active": (df.std_active > 0).astype(int),
+                # TODO: the following features require more inspection
+                "has_fiat": (df.max_fiat > 0).astype(int),
+                "has_biat": (df.max_biat > 0).astype(int),
+                "has_min_flowiat": (df["min_flowiat"] >= 0).astype(int),
+                "has_std_flowiat": (df["std_flowiat"] > 0).astype(int),
+                "has_mean_fiat": (df["mean_fiat"] > 0).astype(int),
+                "has_mean_biat": (df["mean_biat"] > 0).astype(int),
+            }
+        )
+
+    def class_fraction(
+        self, df_num: pd.DataFrame, df_den: pd.DataFrame | None = None
+    ) -> pd.DataFrame:
+        if df_den is None:
+            df_den = self.orig
+
+        population = df_num.groupby("cls").size()
+        population_den = df_den.groupby("cls").size()
+        whole_population = population.sum()
+        whole_population_den = population_den.sum()
+        population_f = (population / whole_population * 100).round(1)
+        fraction = (population / population_den * 100).round(1)
+
+        return pd.concat(
+            [
+                pd.DataFrame(
+                    {
+                        "pop": population,
+                        "pop_f": population_f,
+                        "f": fraction,
+                    }
+                ).rename(self.cls_id_repr_map),
+                pd.DataFrame(
+                    data=[
+                        (
+                            whole_population,
+                            100,
+                            round(whole_population / whole_population_den * 100, 1),
+                        )
+                    ],
+                    columns=pd.Index(["pop", "pop_f", "f"]),
+                    index=pd.Index(["all"]),
+                ),
+            ]
+        )
+
+
+def describe(df: pd.DataFrame):
+    desc = df.describe()
+    desc_t = desc.transpose()
+
+    return (
+        desc.apply(np.log)
+        .map(lambda x: -np.inf if x == -np.inf else x * 10 // 1 / 10)
+        .assign(clas=desc["cls"])
+        .transpose()
+        .assign(count=desc_t["count"])
+    )
+
+
+def positive(df: pd.DataFrame):
+    cond = (df > 0).apply("any", axis=1)
+    return df.loc[cond]
--- src/ml_project/helpers.py
+++ src/ml_project/helpers.py
@@ -1,98 +1,122 @@
+from functools import cached_property
+
 import numpy as np
 import pandas as pd
 
 VAL_TYPES = [-1, 0, 1]
 
 
-def to_val_types(df):
-    return (
-        df.map(lambda x: -1 if x < 0 else 0 if x == 0 else 1)
-        .value_counts()
-        .reset_index()
-    )
+class ValTypes:
+    def __init__(self, df: pd.DataFrame, ignore: list[str] = []):
+        self.df = df
+        self.ignore = ignore
 
+    def df_of_type(self, idx):
+        typ = self.types.drop(columns="count").iloc[idx]
+        cond = True
+        for feat in typ.index:
+            val = typ[feat]
+            if val < 0:
+                cond = (self.df[feat] < 0) & cond
+            elif val == 0:
+                cond = (self.df[feat] == 0) & cond
+            else:
+                cond = (self.df[feat] > 0) & cond
+        return self.df.loc[cond]
 
-def get_connections(val_types):
-    """returns dictionary: if_feat => if_val => then_val => then_feat"""
+    @cached_property
+    def types(self):
+        return (
+            self.df.drop(columns=self.ignore)
+            .map(lambda x: -1 if x < 0 else 0 if x == 0 else 1)
+            .value_counts()
+            .reset_index()
+        )
 
-    df = val_types
-    features = df.drop(columns="count")
+    @cached_property
+    def types_counts(self):
+        return (
+            self.types.melt(
+                id_vars=["count"], value_vars=list(set(self.types.columns) - {"count"})
+            )
+            .groupby(["variable", "value"])
+            .sum()
+        )
 
-    connections = {}
+    @cached_property
+    def unique_types(self):
+        return (
+            self.types.drop(columns="count")
+            .transpose()
+            .apply(lambda x: x.unique(), axis=1)
+        )
 
-    for feature in features:
-        imp = {}
+    @cached_property
+    def impls(self):
+        """returns dictionary: if_feat => if_val => then_val => then_feat"""
 
-        for val in VAL_TYPES:
+        df = self.types
+        feats = df.drop(columns="count")
+
+        impls = {}
+
+        for feat in feats:
+            imp = {}
+
+            for val in VAL_TYPES:
+                for implied_val in VAL_TYPES:
+                    d = df.loc[df[feat] == val].apply(lambda x: x == implied_val)
+                    if d.shape[0] == 0:
+                        continue
+                    implied = d.all()
+                    implied[feat] = False
+                    imp_val = imp.setdefault(val, {})
+                    implied_list = list(implied[implied].index)
+                    if not implied_list:
+                        continue
+                    imp_val[implied_val] = implied_list
+            impls[feat] = imp
+        return impls
+
+    @cached_property
+    def impls_df(self):
+        """turns the connections dictionary into dataframe"""
+
+        tuples = []
+
+        for f, i1 in self.impls.items():
+            for v, i2 in i1.items():
+                for vc, fcs in i2.items():
+                    for fc in fcs:
+                        tuples.append((f, v, fc, vc))
+
+        return pd.DataFrame(
+            tuples, columns=pd.Index(["if_feat", "if_val", "then_feat", "then_val"])
+        )
+
+    @cached_property
+    def eq_groups(self):
+        """returns a list of equivalence classes based on the connections dictionary"""
+
+        def get_connected(feature, val, implied_val):
+            return self.impls.get(feature, {}).get(val, {}).get(implied_val, [])
+
+        eq_groups = []
+
+        to_check = {(feature, value) for feature in self.impls for value in VAL_TYPES}
+
+        while to_check:
+            fv = to_check.pop()
+            feat, val = fv
+            eqs = {fv}
+
             for implied_val in VAL_TYPES:
-                d = df.loc[df[feature] == val].apply(lambda x: x == implied_val)
-                if d.shape[0] == 0:
-                    continue
-                implied = d.all()
-                implied[feature] = False
-                imp_val = imp.setdefault(val, {})
-                implied_list = list(implied[implied].index)
-                if not implied_list:
-                    continue
-                imp_val[implied_val] = implied_list
-        connections[feature] = imp
-    return connections
+                for connected_feat in get_connected(feat, val, implied_val):
+                    if feat in get_connected(connected_feat, implied_val, val):
+                        to_check.discard((connected_feat, implied_val))
+                        eqs.add((connected_feat, implied_val))
+            if len(eqs) <= 1:
+                continue
+            eq_groups.append(eqs)
 
-
-def get_connections_df(connections):
-    """turns the connections dictionary into dataframe"""
-
-    tuples = []
-
-    for f, i1 in connections.items():
-        for v, i2 in i1.items():
-            for vc, fcs in i2.items():
-                for fc in fcs:
-                    tuples.append((f, v, fc, vc))
-
-    return pd.DataFrame(tuples, columns=["if_feat", "if_val", "then_feat", "then_val"])
-
-
-def get_connected(connections, feature, val, implied_val):
-    return connections.get(feature, {}).get(val, {}).get(implied_val, [])
-
-
-def get_connection_groups(connections):
-    """returns a list of equivalence classes based on the connections dictionary"""
-
-    eq_groups = []
-
-    to_check = {(feature, value) for feature in connections for value in VAL_TYPES}
-
-    while to_check:
-        fv = to_check.pop()
-        feature, val = fv
-        eqs = {fv}
-
-        for implied_val in VAL_TYPES:
-            for connected_feature in get_connected(
-                connections, feature, val, implied_val
-            ):
-                if feature in get_connected(
-                    connections, connected_feature, implied_val, val
-                ):
-                    to_check.discard((connected_feature, implied_val))
-                    eqs.add((connected_feature, implied_val))
-        if len(eqs) <= 1:
-            continue
-        eq_groups.append(eqs)
-
-    return sorted(eq_groups)
-
-
-def describe(df: pd.DataFrame):
-    desc = df.describe()
-    desc_t = desc.transpose()
-
-    return (
-        desc.apply(np.log)
-        .map(lambda x: -np.inf if x == -np.inf else x * 10 // 1 / 10)
-        .assign(clas=desc["clas"])
-        .transpose()
-        .assign(count=desc_t["count"])
-    )
+        return sorted(eq_groups)
